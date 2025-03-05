a2d41ff65ed067ba94e0418f03132c2570822f01
nngtkhngoc <khanhngoc230705@gmail.com>
2025-03-05 20:04:05 +0700

    user api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,15 @@
+import bcrypt from "bcryptjs";
+import {
+  signUpValidator,
+  resetPasswordValidator,
+} from "../validation/authValidation.js";
 import { prisma } from "../config/db.js";
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
+import generateToken from "../utils/generateToken.js";
+import {
+  sendVerificationEmail,
+  sendResetPasswordEmail,
+} from "../config/nodemailer.js";
 
 export const getAllUsers = async (req, res) => {
   try {
@@ -6,6 +17,313 @@ export const getAllUsers = async (req, res) => {
 
     return res.status(200).json({ success: true, data: users });
   } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const user_id = req.user_id;
+  try {
+    const user = await prisma.users.findUnique({
+      omit: {
+        password: true,
+      },
+      where: { user_id },
+    });
+
+    if (user) {
+      return res.status(200).json({ success: true, data: user });
+    }
+
+    return res.status(404).json({ success: false, message: "User not found" });
+  } catch (error) {
+    console.log("Error get user: ", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const signUp = async (req, res) => {
+  const data = req.body;
+
+  try {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No data provided" });
+    }
+
+    await signUpValidator.validateAsync(data);
+
+    const [checkUsername, checkEmail, checkPhoneNumber] = await Promise.all([
+      await prisma.users.findUnique({ where: { username: data.username } }),
+      await prisma.users.findUnique({ where: { email: data.email } }),
+      await prisma.users.findUnique({
+        where: { phone_number: data.phone_number },
+      }),
+    ]);
+
+    if (checkEmail) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    if (checkUsername) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Username already exists" });
+    }
+
+    if (checkPhoneNumber) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Phone number already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const newUser = await prisma.users.create({
+      data: {
+        username: data.username,
+        phone_number: data.phone_number,
+        email: data.email,
+        password: hashedPassword,
+        role: data.role ? data.role : "USER",
+      },
+    });
+
+    generateTokenAndSetCookie(newUser.user_id, newUser.role, res);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Sign up sucesfully", data: newUser });
+  } catch (error) {
+    if (error.isJoi) {
+      return res.status(400).json({
+        success: false,
+        message: error.details.map((err) => err.message),
+      });
+    }
+    console.log("Error signing up: ", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  const { identifier, password } = req.body;
+  try {
+    if (!identifier || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide identifier and password",
+      });
+    }
+
+    const user = await prisma.users.findFirst({
+      where: {
+        OR: [
+          { email: identifier },
+          { phone_number: identifier },
+          { username: identifier },
+        ],
+      },
+    });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(406)
+        .json({ success: false, message: "Incorrect password" });
+    }
+
+    generateTokenAndSetCookie(user.user_id, user.role, res);
+
+    return res.json({
+      success: true,
+      data: {
+        username: user.username,
+        fullname: user.fullname,
+        email: user.email,
+        phone_number: user.phone_number,
+        profile_pic: user.profile_pic,
+      },
+    });
+  } catch (error) {
+    console.log("Error sign in:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const signOut = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Sign out successfully" });
+  } catch (error) {
+    console.log("Error sign out:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getVerificationToken = async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const user = await prisma.users.findUnique({ where: { user_id } });
+
+    if (user) {
+      if (!user.is_verified) {
+        const verification_token = generateToken();
+
+        await prisma.users.update({
+          where: { user_id },
+          data: {
+            verification_token: verification_token,
+            verification_token_expires_at: new Date(Date.now() + 5 * 60 * 1000),
+          },
+        });
+
+        await sendVerificationEmail(user.email, verification_token);
+
+        return res.status(200).json({ message: "Send Token successfully" });
+      } else {
+        return res.status(400).json({ message: "User has already verified" });
+      }
+    }
+
+    return res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    console.log("Error send token", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { verification_token } = req.body;
+
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        verification_token,
+        verification_token_expires_at: { gt: new Date() },
+      },
+    });
+
+    if (user) {
+      await prisma.users.update({
+        where: { verification_token },
+        data: {
+          is_verified: true,
+          verification_token: null,
+          verification_token_expires_at: null,
+        },
+      });
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Email verified successfuly" });
+    }
+
+    return res.status(404).json({ success: false, message: "Invalid token" });
+  } catch (error) {
+    console.log("Error verify email:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internet Server Error" });
+  }
+};
+
+export const getResetPasswordToken = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+
+    const reset_password_token = generateToken();
+
+    await prisma.users.update({
+      where: { email },
+      data: {
+        reset_password_token: reset_password_token,
+        reset_password_token_expires_at: new Date(Date.now() + 5 * 60 * 1000),
+      },
+    });
+
+    await sendResetPasswordEmail(user.email, reset_password_token);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Send mail succesfully" });
+  } catch (error) {
+    console.log("Error send reset password mail:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { reset_password_token } = req.params;
+  const { new_password, confirm_new_password } = req.body;
+  try {
+    await resetPasswordValidator.validateAsync(req.body);
+
+    const user = await prisma.users.findUnique({
+      where: {
+        reset_password_token,
+        reset_password_token_expires_at: { gt: new Date() },
+      },
+    });
+
+    if (user) {
+      const hashedPassword = await bcrypt.hash(new_password, 10);
+
+      await prisma.users.update({
+        where: { reset_password_token },
+        data: {
+          password: hashedPassword,
+          reset_password_token: null,
+          reset_password_token_expires_at: null,
+        },
+      });
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Reset password successfully" });
+    }
+
+    return res.status(400).json({ sucess: false, message: "Invalid token" });
+  } catch (error) {
+    if (error.isJoi) {
+      return res.status(400).json({
+        success: false,
+        message: error.details.map((err) => err.message),
+      });
+    }
+    console.log("Error reset password: ", error);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error" });
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,30 @@
-import { getAllUsers } from "../controllers/authController.js";
-
+import {
+  signUp,
+  signIn,
+  getUser,
+  signOut,
+  getAllUsers,
+  verifyEmail,
+  getVerificationToken,
+  getResetPasswordToken,
+  resetPassword,
+} from "../controllers/authController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { verifyAdmin } from "../middleware/verifyAdmin.js";
 import express from "express";
+
 const router = express.Router();
 
-router.get("/get-all", getAllUsers);
+router.get("/me", verifyToken, getUser);
+router.get("/get-all", verifyToken, verifyAdmin, getAllUsers);
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.post("/sign-out", signOut);
+
+router.get("/get-verification-token/:user_id", getVerificationToken);
+router.post("/verify", verifyEmail);
+
+router.post("/get-reset-password-token", getResetPasswordToken);
+router.post("/reset-password/:reset_password_token", resetPassword);
 
 export default router;
