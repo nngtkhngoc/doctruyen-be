ffad4a77388c4eb2dd287c5e4dafc2573de03b09
unknown <ungquangtri@gmail.com>
2025-04-13 15:03:51 +0700

    Handle chatbot api

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ import genreRouter from "./genreRouter.js";
 import storyRouter from "./storyRouter.js";
 import chapterRouter from "./chapterRouter.js";
 import blogRouter from "./blogRouter.js";
-
+import chatbotRouter from "./chatbotRouter.js";
 export const routes = (app) => {
   app.use("/api/auth", authRouter);
   app.use("/api/genres", genreRouter);
   app.use("/api/stories", storyRouter);
   app.use("/api/chapters", chapterRouter);
   app.use("/api/blogs", blogRouter);
+  app.use("/api/chatbot", chatbotRouter);
 };
