4152221ac81bdc40bcdef8dddf6f26ea45bc5e1f
unknown <ungquangtri@gmail.com>
2025-05-15 13:07:31 +0700

    Add github actions workflow

--- routes/chapterRouter.js
+++ routes/chapterRouter.js
@@ -4,15 +4,16 @@ import {
   deleteChapter,
   getChapter,
   updateChapter,
+  importExcel,
 } from "../controllers/chapterController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js";
-
+import { upload } from "../middleware/multer.js";
 const router = express.Router();
 
 router.get("/:chapter_id", getChapter);
 router.post("/", verifyToken, verifyAdmin, createChapter);
 router.delete("/:chapter_id", verifyToken, verifyAdmin, deleteChapter);
 router.put("/:chapter_id", verifyToken, verifyAdmin, updateChapter);
-
+router.post("/import-excel", upload.single("file"), importExcel);
 export default router;
